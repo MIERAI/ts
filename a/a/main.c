5fc56cfc817514b615ee9be0f0c481c9a2c98eac
zaiqi yang <zaiqiyang@zaiqis-MacBook-Air.local>
2022-08-23 00:37:08 +0900

    新建a文件

---
+++ a/a/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  a
+//
+//  Created by zaiqi yang on 2022/08/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
